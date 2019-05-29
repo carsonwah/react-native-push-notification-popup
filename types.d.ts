ad18e35b5853386bfa2d2bf02e0f01962b3988f7
Daniel Holmes <daniel@danielholmes.org>
2019-05-29 13:29:29 +1000

    Add typescript typings

---
+++ types.d.ts
@@ -0,0 +1,16 @@
+declare module 'react-native-push-notification-popup' {
+  import { Component } from 'react'
+
+  interface ShowOptions {
+    onPress?: () => void;
+    appIconSource?: string;
+    appTitle?: string;
+    timeText?: string;
+    title?: string;
+    body?: string;
+  }
+
+  export default class ReactNativePushNotificationPopup extends Component {
+    public show(options: ShowOptions): void;
+  }
+}
