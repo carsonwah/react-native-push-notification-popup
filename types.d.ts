6b24e93c01cc94c82bf35cd88fecda42d3bc4c2e
Daniel Holmes <daniel@danielholmes.org>
2019-05-30 11:06:20 +1000

    Add ability to pass custom styles in

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,6 @@
 declare module 'react-native-push-notification-popup' {
   import { Component } from 'react'
+  import { ViewStyle, ImageStyle, TextStyle } from 'react-native'
 
   interface ShowOptions {
     onPress?: () => void;
@@ -11,7 +12,27 @@ declare module 'react-native-push-notification-popup' {
     slideOutTime?: number;
   }
 
-  export default class ReactNativePushNotificationPopup extends Component {
+  interface Style {
+    popupContainer?: ViewStyle;
+    popupHeaderContainer?: ViewStyle;
+    headerIconContainer?: ViewStyle;
+    headerIcon?: ImageStyle;
+    headerTextContainer?: ViewStyle;
+    headerText?: TextStyle;
+    headerTimeContainer?: ViewStyle;
+    headerTime?: TextStyle;
+    contentContainer?: ViewStyle;
+    contentTitleContainer?: ViewStyle;
+    contentTitle?: TextStyle;
+    contentTextContainer?: ViewStyle;
+    contentText?: TextStyle;
+  }
+
+  interface Props {
+    style?: Style
+  }
+
+  export default class ReactNativePushNotificationPopup extends Component<Props> {
     public show(options: ShowOptions): void;
   }
 }
