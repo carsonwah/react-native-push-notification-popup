304604ba9ede66d42349810463f71adbdd5887e5
Sung-Youb Andre Song <sungsong88@users.noreply.github.com>
2023-02-06 11:19:49 -0500

    iPhone14 Pro and Pro Max Dynamic Island SafeArea support

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,6 +5,8 @@ const STATUSBAR_DEFAULT_HEIGHT = 20;
 const STATUSBAR_X_HEIGHT = 44;
 const STATUSBAR_IP12_HEIGHT = 47;
 const STATUSBAR_IP12MAX_HEIGHT = 47;
+const STATUSBAR_IP14PRO_HEIGHT = 54;
+const STATUSBAR_IP14PROMAX_HEIGHT = 54;
 
 const X_WIDTH = 375;
 const X_HEIGHT = 812;
@@ -18,6 +20,12 @@ const IP12_HEIGHT = 844;
 const IP12MAX_WIDTH = 428;
 const IP12MAX_HEIGHT = 926;
 
+const IP14PRO_WIDTH = 393;
+const IP14PRO_HEIGHT = 852;
+
+const IP14PROMAX_WIDTH = 430;
+const IP14PROMAX_HEIGHT = 932;
+
 const { height: W_HEIGHT, width: W_WIDTH } = Dimensions.get("window");
 
 let statusBarHeight = STATUSBAR_DEFAULT_HEIGHT;
@@ -25,6 +33,8 @@ let isIPhoneX_v = false;
 let isIPhoneXMax_v = false;
 let isIPhone12_v = false;
 let isIPhone12Max_v = false;
+let isIPhone14Pro_v = false;
+let isIPhone14ProMax_v = false;
 let isIPhoneWithMonobrow_v = false;
 
 if (Platform.OS === "ios" && !Platform.isPad && !Platform.isTVOS) {
@@ -44,6 +54,14 @@ if (Platform.OS === "ios" && !Platform.isPad && !Platform.isTVOS) {
         isIPhoneWithMonobrow_v = true;
         isIPhone12Max_v = true;
         statusBarHeight = STATUSBAR_IP12MAX_HEIGHT;
+    } else if (W_WIDTH === IP14PRO_WIDTH && W_HEIGHT === IP14PRO_HEIGHT) {
+        isIPhoneWithMonobrow_v = true;
+        isIPhone14Pro_v = true;
+        statusBarHeight = STATUSBAR_IP14PRO_HEIGHT;
+    } else if (W_WIDTH === IP14PROMAX_WIDTH && W_HEIGHT === IP14PROMAX_HEIGHT) {
+        isIPhoneWithMonobrow_v = true;
+        isIPhone14ProMax_v = true;
+        statusBarHeight = STATUSBAR_IP14PROMAX_HEIGHT;
     }
 }
 
