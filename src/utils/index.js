854b432481db1bac7d16f4c9486b48e6f2067190
Carson Wah <carsonwah@gmail.com>
2023-08-17 23:21:16 +0800

    Fix code smell (#58)
    * Add new props to support non-translucent android statusbar
    
    * Document new props in readme
    
    * Add editorconfig
    
    * Run eslint with auto-fix
    
    * Remove unused variables

--- src/utils/index.js
+++ src/utils/index.js
@@ -26,50 +26,30 @@ const IP14PRO_HEIGHT = 852;
 const IP14PROMAX_WIDTH = 430;
 const IP14PROMAX_HEIGHT = 932;
 
-const { height: W_HEIGHT, width: W_WIDTH } = Dimensions.get("window");
+const { height: W_HEIGHT, width: W_WIDTH } = Dimensions.get('window');
 
 let statusBarHeight = STATUSBAR_DEFAULT_HEIGHT;
-let isIPhoneX_v = false;
-let isIPhoneXMax_v = false;
-let isIPhone12_v = false;
-let isIPhone12Max_v = false;
-let isIPhone14Pro_v = false;
-let isIPhone14ProMax_v = false;
-let isIPhoneWithMonobrow_v = false;
 
-if (Platform.OS === "ios" && !Platform.isPad && !Platform.isTVOS) {
-    if (W_WIDTH === X_WIDTH && W_HEIGHT === X_HEIGHT) {
-        isIPhoneWithMonobrow_v = true;
-        isIPhoneX_v = true;
-        statusBarHeight = STATUSBAR_X_HEIGHT;
-    } else if (W_WIDTH === XSMAX_WIDTH && W_HEIGHT === XSMAX_HEIGHT) {
-        isIPhoneWithMonobrow_v = true;
-        isIPhoneXMax_v = true;
-        statusBarHeight = STATUSBAR_X_HEIGHT;
-    } else if (W_WIDTH === IP12_WIDTH && W_HEIGHT === IP12_HEIGHT) {
-        isIPhoneWithMonobrow_v = true;
-        isIPhone12_v = true;
-        statusBarHeight = STATUSBAR_IP12_HEIGHT;
-    } else if (W_WIDTH === IP12MAX_WIDTH && W_HEIGHT === IP12MAX_HEIGHT) {
-        isIPhoneWithMonobrow_v = true;
-        isIPhone12Max_v = true;
-        statusBarHeight = STATUSBAR_IP12MAX_HEIGHT;
-    } else if (W_WIDTH === IP14PRO_WIDTH && W_HEIGHT === IP14PRO_HEIGHT) {
-        isIPhoneWithMonobrow_v = true;
-        isIPhone14Pro_v = true;
-        statusBarHeight = STATUSBAR_IP14PRO_HEIGHT;
-    } else if (W_WIDTH === IP14PROMAX_WIDTH && W_HEIGHT === IP14PROMAX_HEIGHT) {
-        isIPhoneWithMonobrow_v = true;
-        isIPhone14ProMax_v = true;
-        statusBarHeight = STATUSBAR_IP14PROMAX_HEIGHT;
-    }
+if (Platform.OS === 'ios' && !Platform.isPad && !Platform.isTVOS) {
+  if (W_WIDTH === X_WIDTH && W_HEIGHT === X_HEIGHT) {
+    statusBarHeight = STATUSBAR_X_HEIGHT;
+  } else if (W_WIDTH === XSMAX_WIDTH && W_HEIGHT === XSMAX_HEIGHT) {
+    statusBarHeight = STATUSBAR_X_HEIGHT;
+  } else if (W_WIDTH === IP12_WIDTH && W_HEIGHT === IP12_HEIGHT) {
+    statusBarHeight = STATUSBAR_IP12_HEIGHT;
+  } else if (W_WIDTH === IP12MAX_WIDTH && W_HEIGHT === IP12MAX_HEIGHT) {
+    statusBarHeight = STATUSBAR_IP12MAX_HEIGHT;
+  } else if (W_WIDTH === IP14PRO_WIDTH && W_HEIGHT === IP14PRO_HEIGHT) {
+    statusBarHeight = STATUSBAR_IP14PRO_HEIGHT;
+  } else if (W_WIDTH === IP14PROMAX_WIDTH && W_HEIGHT === IP14PROMAX_HEIGHT) {
+    statusBarHeight = STATUSBAR_IP14PROMAX_HEIGHT;
+  }
 }
 
 export function getStatusBarHeight() {
-    return Platform.select({
-        ios: statusBarHeight,
-        android: StatusBar.currentHeight,
-        default: 0,
-    });
+  return Platform.select({
+    ios: statusBarHeight,
+    android: StatusBar.currentHeight,
+    default: 0,
+  });
 }
-
